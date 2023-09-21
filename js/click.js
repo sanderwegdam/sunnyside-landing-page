aa846528e3ae21b6669f6dea95e43cc6acf0efd4
nouser <nouser>
2023-09-21 14:24:44 +0200

    update

---
+++ js/click.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const iconHamburger = document.querySelector('.icon-hamburger');
+  const menu = document.querySelector('.menu');
+  const menuLinks = menu.querySelectorAll('a'); // Alle links binnen het menu ophalen
+
+  iconHamburger.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    if (menu.classList.contains('menu-open')) {
+      // Als het menu al open is, sluit het
+      menu.classList.remove('menu-open');
+      menu.classList.add('menu');
+    } else {
+      // Anders open het menu      
+      menu.classList.add('menu-open');
+    }
+  });
+
+  // Event listeners toevoegen aan de links binnen het menu
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault(); // Voorkom standaardgedrag van de link
+      menu.classList.remove('menu-open');
+      menu.classList.add('menu');
+    });
+  });
+
+  // Event listener toevoegen aan het document om klikken buiten het menu te detecteren
+  document.addEventListener('click', function (e) {
+    if (!menu.contains(e.target) && !iconHamburger.contains(e.target)) {
+      // Als er buiten het menu en buiten het hamburgerpictogram wordt geklikt
+      menu.classList.remove('menu-open');
+      menu.classList.add('menu');
+    }
+  });
+});
